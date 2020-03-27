8fb00ef66e3526ab2b9fc13714a2e8d925864d3a
Akshata Bhat <mail.akshata18@gmail.com>
2020-03-26 22:04:38 -0500

    More evaluation - doc and context level retrieval

--- natural_questions/convert_NQ_to_jsonl.py
+++ natural_questions/convert_NQ_to_jsonl.py
@@ -3,8 +3,10 @@ import pandas as pd
 import argparse
 import os 
 
-def convert_NQ(args):
-    print('Converting NQ...')
+def convert_NQ_elements(args):
+    if not os.path.exists(args.output_folder+'_ele'):
+        os.makedirs(args.output_folder+'_ele')
+    print('Converting NQ contexts...')
     file_index = 0
     document_df = pd.read_hdf(args.collection_path, key="elements")
     i = 0
@@ -17,7 +19,7 @@ def convert_NQ(args):
         if i % args.max_docs_per_file == 0:
             if i > 0:
                 output_jsonl_file.close()
-            output_path = os.path.join(args.output_folder, 'docs{:02d}.json'.format(file_index))
+            output_path = os.path.join(args.output_folder+'_ele', 'docs{:02d}.json'.format(file_index))
             output_jsonl_file = open(output_path, 'w', encoding='utf-8', newline='\n')
             file_index += 1
         output_dict = {'id': str(i), 'contents': content}
@@ -27,21 +29,48 @@ def convert_NQ(args):
             print('Converted {} docs in {} files'.format(i, file_index))
         i += 1
     output_jsonl_file.close()
-    with open(args.index_table+'/index_table.json', 'w') as fp:
+    with open(args.index_table+'/index_table_ele.json', 'w') as fp:
         json.dump(index_table, fp)
+
+def convert_NQ_docs(args):
+    if not os.path.exists(args.output_folder+'_doc'):
+        os.makedirs(args.output_folder+'_doc')
+    print('Converting NQ ...')
+    file_index = 0
+    document_df = pd.read_hdf(args.collection_path, key="documents")
+    i = 0
+    index_table = {}
+    for index, row in document_df.iterrows():
+        doc_id = row["doc_id"]
+        content = row["document"]
+        index_table[i] = {"doc_id":doc_id, "document": content}
+        if i % args.max_docs_per_file == 0:
+            if i > 0:
+                output_jsonl_file.close()
+            output_path = os.path.join(args.output_folder+'_doc', 'docs{:02d}.json'.format(file_index))
+            output_jsonl_file = open(output_path, 'w', encoding='utf-8', newline='\n')
+            file_index += 1
+        output_dict = {'id': str(i), 'contents': content}
+        output_jsonl_file.write(json.dumps(output_dict) + '\n')
+
+        if i % 100000 == 0:
+            print('Converted {} docs in {} files'.format(i, file_index))
+        i += 1
+    output_jsonl_file.close()
+    with open(args.index_table+'/index_table_doc.json', 'w') as fp:
+        json.dump(index_table, fp)
+
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='''Converts processedx NQ to Anserini jsonl files.''')
     parser.add_argument('--collection_path', required=True, help='NQ .h5 file')
     parser.add_argument('--output_folder', required=True, help='output filee')
-    parser.add_argument('--index_table', required=True, help='index table filee')
+    parser.add_argument('--index_table', required=True, help='index table file')
     parser.add_argument('--max_docs_per_file', default=1000000, type=int, help='maximum number of documents in each jsonl file.')
 
     args = parser.parse_args()
 
-    if not os.path.exists(args.output_folder):
-        os.makedirs(args.output_folder)
-
-    convert_NQ(args)
+    convert_NQ_elements(args)
+    convert_NQ_docs(args)
     print('Done!')
     
 
--- natural_questions/evaluate_NQ.py
+++ natural_questions/evaluate_NQ.py
@@ -3,8 +3,41 @@ import json
 import pandas as pd
 import argparse
 
-def evaluate(examples_df, index_table, k):
-    print("Begin evaluation")
+def evaluate1(examples_df, args):
+    with open(args.input_dir + '/index_table_doc.json') as f:
+        index_table = json.load(f)
+    k1 = args.k1
+    searcher = pysearch.SimpleSearcher(args.input_dir +'/lucene-index-nq-doc')
+    print("Begin evaluation 1")
+    correct = 0
+    num_of_examples = 0
+    for index, example in examples_df.iterrows():
+        num_of_examples += 1
+        question = example["query"]
+        actual_doc_id = example["doc_id"]
+        #print("******************** Example id : ", num_of_examples, "***************")
+        #print("question : ", question)
+        #print("context : ")
+        # Search using anserini
+        hits = searcher.search(question, k=k1)
+        for i in range(0, k1):
+            index = hits[i].docid
+            #print(hits[i].content)
+            #print(i, ":", index_table[index]["content"])
+            #print("score : ", hits[i].docid)
+            if index_table[index]["doc_id"] == actual_doc_id:
+                correct +=1
+                break
+    print("Total number of examples : ", num_of_examples)
+    recall = correct/num_of_examples
+    print("Proportion of correct retrieval : %f" % (recall))  
+
+def evaluate2(examples_df, args):
+    with open(args.input_dir + '/index_table_ele.json') as f:
+        index_table = json.load(f)
+    k2 = args.k2
+    searcher = pysearch.SimpleSearcher(args.input_dir +'/lucene-index-nq-ele')
+    print("Begin evaluation 2")
     correct = 0
     num_of_examples = 0
     for index, example in examples_df.iterrows():
@@ -16,29 +49,63 @@ def evaluate(examples_df, index_table, k):
         #print("question : ", question)
         #print("context : ")
         # Search using anserini
-        hits = searcher.search(question, k=k)
-        for i in range(0, k):
+        hits = searcher.search(question, k=k2)
+        for i in range(0, k2):
             index = hits[i].docid
+            #print(hits[i].content)
             #print(i, ":", index_table[index]["content"])
             #print("score : ", hits[i].docid)
             if index_table[index]["doc_id"] == actual_doc_id and index_table[index]["element_id"] == actual_element_id:
-                correct +=1
-                break
+                 correct +=1
+                 break
     print("Total number of examples : ", num_of_examples)
     recall = correct/num_of_examples
     print("Proportion of correct retrieval : %f" % (recall))       
 
+def evaluate3(examples_df, args):
+    # TODO: This doesn't make sense !!!
+    with open(args.input_dir + '/index_table_doc.json') as f:
+        index_table_doc = json.load(f)
+    with open(args.input_dir + '/index_table_ele.json') as f:
+        index_table_ele = json.load(f)
+    k1 = args.k1
+    k2 = args.k2
+    searcher1 = pysearch.SimpleSearcher(args.input_dir +'/lucene-index-nq-doc')
+    searcher2 = pysearch.SimpleSearcher(args.input_dir +'/lucene-index-nq-ele')
+    print("Begin evaluation 3")
+    correct = 0
+    num_of_examples = 0
+    for index, example in examples_df.iterrows():
+        num_of_examples += 1
+        question = example["query"]
+        actual_element_id = example["context_id"]
+        actual_doc_id = example["doc_id"]
+
+        doc_hits = searcher1.search(question, k=k1)
+        ele_hits = searcher2.search(question, k=k2)
+        doc_ids = []
+        
+        for i in range(0, k1):
+            index = doc_hits[i].docid
+            doc_ids.append(index_table_doc[index]["doc_id"])
+        for i in range(0, k2):
+            index = ele_hits[i].docid
+            if index_table_ele[index]["doc_id"] in doc_ids and index_table_ele[index]["doc_id"] == actual_doc_id and index_table_ele[index]["element_id"] == actual_element_id:
+                correct +=1
+                break
+
+    print("Total number of examples : ", num_of_examples)
+    recall = correct/num_of_examples
+    print("Proportion of correct retrieval : %f" % (recall))  
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='''Converts processedx NQ to Anserini jsonl files.''')
     parser.add_argument('--input_dir', required=True, help='path to input directory')
-    parser.add_argument('--k', required=True, type=int, help='number of documents to be returned by search')
+    parser.add_argument('--k1', required=True, type=int, help='number of documents to be returned by search')
+    parser.add_argument('--k2', required=True, type=int, help='number of documents to be returned by search')
     args = parser.parse_args()
 
     examples_df = pd.read_hdf(args.input_dir + "/NQ.h5", key="examples")
-    with open(args.input_dir + '/index_table.json') as f:
-        index_table = json.load(f)
-
-    searcher = pysearch.SimpleSearcher(args.input_dir + '/lucene-index-msmarco/')
-
-    evaluate(examples_df, index_table, args.k)
+    evaluate1(examples_df, args)
+    evaluate2(examples_df, args)
+    evaluate3(examples_df, args)
